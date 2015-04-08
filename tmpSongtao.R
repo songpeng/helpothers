9f01f99718b91e7bed7d3fb4c830d6ebf2a7fae2
zusp <zusongpeng@gmail.com>
2015-04-08 23:59:58 +0800

    Update tmpSongtao.R

--- tmpSongtao.R
+++ tmpSongtao.R
@@ -7,5 +7,13 @@ library(xlsx)
 
 #-- load data
 # Note: before reading, change the sheet names from Chinese to English.
-tmp <- read.xlsx("datafromtao.xlsx",3,Encoding = "UTF-8",startRow = 1,
-                 endRow = 1,header=FALSE)
+# Get the first line to determine the column number.
+firstl <- read.xlsx("datafromtao.xlsx",3,Encoding = "UTF-8",startRow = 1,
+                    endRow = 1,header=FALSE, stringsAsFactors = FALSE)
+setcolclass <- c("Date", rep("numeric",length(firstl)-1))
+# Get the first column
+firstc <- read.xlsx("datafromtao.xlsx",3,colClasses="Date", colIndex=1,
+                    stringsAsFactors=FALSE)
+# Read the data part.
+incomedata <- read.xlsx("datafromtao.xlsx",3,startRow=4,header=FALSE,
+                        colClasses=setcolclass)
