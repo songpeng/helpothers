2d44b6dad37caeff0be607cf3842831e5236c0ec
zusp <zusongpeng@gmail.com>
2015-06-03 20:38:59 +0800

    vectorize the computation.

--- TCMCompound/compoundsIdentify.R
+++ TCMCompound/compoundsIdentify.R
@@ -14,6 +14,7 @@ cidsNotSure <- read.xlsx("CompoundsIdentify_150528.xlsx",2,header=FALSE,
 cidsAll <- read.xlsx("CompoundsIdentify_150528.xlsx",1,header=FALSE,
                      colIndex = 1)
 cidsAllothers <- setdiff(cidsAll[[1]],cidsNotSure[[1]])
+
 #-- Calculate the maximum comman structure similarity.
 compoundsNotSure <- getIds(cidsNotSure[[1]])
 compoundsAllOthers <- getIds(cidsAllothers)
@@ -21,43 +22,28 @@ compoundsAllOthers <- getIds(cidsAllothers)
 nnrow <- length(compoundsNotSure)
 nncol <- length(compoundsAllOthers)
 
-MCSmat <- matrix(,nrow = nnrow, ncol = nncol)
-
-MCScalcByRow <- function(rowindex){
-    tmparray <- 1:nncol
-    for(j in 1:nncol){
-        tmp <- fmcs(compoundsNotSure[rowindex],compoundsAllOthers[j])
-        tmparray[j] <- tmp@stats[4]
-    }
-    return(tmparray)
-}
-
-for(i in 1:nnrow){
-    max(MCScalcByRow(i))
-}
-mc <- getOption("mc.cores",3)
-test <- mclapply(1:nnrow,MCScalcByRow,mc.cores=mc)
-
-MCScalcByEle <- function(rowindex,colindex){
-    tmp <- fmcs(compoundsNotSure[rowindex],compoundsAllOthers[colindex])
-    return(tmp@stats[4])
-}
-test <- outer(1:nnrow,1:nncol,FUN="MCScalcByEle")
-test <- mapply(function(r,c) MCScalcByEle(r,c),1:nnrow,1:nncol)
+#MCSmat <- matrix(,nrow = nnrow, ncol = nncol)
+#MCScalcByRow <- function(rowindex){
+#    tmparray <- 1:nncol
+#    for(j in 1:nncol){
+#        tmp <- fmcs(compoundsNotSure[rowindex],compoundsAllOthers[j])
+#        tmparray[j] <- tmp@stats[4]
+#    }
+#    return(tmparray)
+#}
+#MCScalcByEle <- function(rowindex,colindex){
+#    tmp <- fmcs(compoundsNotSure[rowindex],compoundsAllOthers[colindex])
+#    return(tmp@stats[4])
+#}
+#test <- outer(1:nnrow,1:nncol,FUN="MCScalcByEle")
+#test <- mapply(function(r,c) MCScalcByEle(r,c),1:nnrow,1:nncol)
 
 col1 <- rep(1:nnrow,nncol)
 col2 <- rep(1:nncol,nnrow)
 
 totallen <- nnrow*nncol
-array <- col1
-for(i in 1:totallen){
-    tmp <- fmcs(compoundsNotSure[col1[i]],compoundsAllOthers[col2[i]])
-    array[i] <- tmp@stats[4]
-}
-
 MCScalcByArray <- function(numindex){
-    tmp <- fmcs(compoundsNotSure[col1[1]],compoundsAllOthers[col2[i]])
-    return(tmp@stats[4])
+    return(fmcs(compoundsNotSure[[col1[i]]],compoundsAllOthers[[col2[i]]],
+                fast=TRUE)[4])
 }
-
 resultsBYarray <- lapply(1:totallen,function(x) MCScalcByArray(x))
