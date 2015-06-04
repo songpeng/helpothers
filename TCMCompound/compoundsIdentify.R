530f0c213f1bffe74b03c1e79e7d1a7949bae7cc
zusp <zusongpeng@gmail.com>
2015-06-04 18:13:05 +0800

    It seems to work now. Use cat and Rcpp.

--- TCMCompound/compoundsIdentify.R
+++ TCMCompound/compoundsIdentify.R
@@ -32,20 +32,21 @@ MCScalcByRow <- function(rowindex){
     return(unlist(lapply(1:nncol,function(x) MCScalcByEle(rowindex,x))))
 }
 
-fileConn <- file("outMCScalc.txt")
+#fileConn <- file("outMCScalc.txt")
+filename <- "outMCScalc.txt"
 CatResult <- function(rowindex){
-#    cat(paste(MCScalcByRow(rowindex),sep="\n",collapse="\t"),
-#        file=filename,append=TRUE,sep="\n")
+    cat(paste(MCScalcByRow(rowindex),collapse="\t"),
+        file=filename,append=TRUE,sep="\n")
     #It seems to be slower than cat.
-    writeLines(paste(MCScalcByRow(rowindex),collapse="\t"),fileConn)
+#    writeLines(paste(MCScalcByRow(rowindex),collapse="\t"),fileConn)
 }
-close(fileConn)
-
 sourceCpp("MCScalc.cpp")
+callFunction(10,CatResult)
+#close(fileConn)
 
-sink("outMCScalc.txt",append=TRUE)
+#sink("outMCScalc.txt",append=TRUE)
 #lapply(1:10,function(x) CatResult(x))
 #sink()
 #use plotMCS to plot the matching results (fmcs without fast parameter).
-callFunction(10,CatResult)
-sink()
+#callFunction(10,CatResult)
+#sink()
